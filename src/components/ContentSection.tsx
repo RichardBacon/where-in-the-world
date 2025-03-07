5d5be5fd6ba0cb53cf97e7bf33774676f013c1a4
RichardBacon <richardjohnbacon@gmail.com>
2025-03-07 10:20:10 +0000

    Add Message component for displaying empty state

--- src/components/ContentSection.tsx
+++ src/components/ContentSection.tsx
@@ -2,7 +2,7 @@ import { CountryCardData } from '../types/Country'
 import CountryGrid from './CountryGrid'
 import ErrorMessage from './ErrorMessage'
 import Loader from './Loader'
-
+import Message from './Message'
 interface ContentSectionProps {
   isLoading: boolean
   error: string | null
@@ -16,6 +16,9 @@ const ContentSection = ({
 }: ContentSectionProps) => {
   if (isLoading) return <Loader />
   if (error) return <ErrorMessage message={error} />
+  if (countries.length === 0) {
+    return <Message text='No countries found' />
+  }
   return <CountryGrid countries={countries} />
 }
 
---
+++ src/components/Message.tsx
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled'
+import useCustomTheme from '../hooks/useCustomTheme'
+
+interface MessageProps {
+  text: string
+}
+
+const MessageContainer = styled.div<{ isDarkMode: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  font-size: 2rem;
+  font-weight: 600;
+  color: ${({ theme, isDarkMode }) =>
+    isDarkMode ? theme.colors.dark.text : theme.colors.light.text};
+
+  @media (min-width: 768px) {
+    font-size: 3.2rem;
+  }
+`
+
+const Message = ({ text }: MessageProps) => {
+  const { isDarkMode } = useCustomTheme()
+  return <MessageContainer isDarkMode={isDarkMode}>{text}</MessageContainer>
+}
+
+export default Message
